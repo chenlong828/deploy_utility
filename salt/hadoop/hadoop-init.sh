f5b302a88dbf0ad84b758e4a23e6259ab37ad413
ChenLong828 <chenlong828@gmail.com>
2013-10-01 23:39:10 +0800

    Add hadoop host init file

---
+++ salt/hadoop/hadoop-init.sh
@@ -0,0 +1,15 @@
+saltmaster_host="saltstack"
+saltmaster_hostip="172.22.241.156"
+host_name=$1
+
+echo $1 > /etc/hostname
+sed -i "s/localhost.cs2cloud.internal/"$host_name"/" /etc/hosts
+
+apt-get install python-software-properties -y
+add-apt-repository ppa:saltstack/salt -y
+apt-get update
+sudo apt-get install salt-minion -y
+sed -i "s/#master: salt/master: "$saltmaster_host"/" /etc/salt/minion
+
+echo $saltmaster_hostip"	"$saltmaster_host >> /etc/hosts
+/etc/init.d/salt-minion restart
